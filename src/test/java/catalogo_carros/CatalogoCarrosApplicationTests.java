f800ae5ef4aa4c193644e62567116730b36fe4bd
Rodrigo-Capello <rodrigo.capello@live.com>
2026-09-22 12:04:38 -0300

    Inicial - Spring Initializr

---
+++ src/test/java/catalogo_carros/CatalogoCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package catalogo_carros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
